4f63a5a49aab6bc1d7de0770c2b186b20e0420ed
sanchesaline6 <sanches.aline6@gmail.com>
2020-11-04 18:01:44 -0300

    adding hook classes

---
+++ src/CoffeWithHook.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CoffeWithHook extends CaffeineBeverageWithHook{
+    public void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    public void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+
+    public boolean customerWantsCondiments() {
+        String answer = getUserInput();
+
+        return answer.toLowerCase().startsWith("y");
+    }
+
+    public String getUserInput() {
+        String answer = null;
+        System.out.println("Would you like milk and sugar with your coffee (y/n)? ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            answer = in.readLine();
+        }catch (IOException ioe) {
+            System.err.println("IO error trying to read your answer");
+        }
+        if(answer == null) {
+            return "no";
+        }
+        return answer;
+    }
+}
--- src/Program.java
+++ src/Program.java
@@ -3,5 +3,13 @@ public class Program {
         Tea myTea = new Tea();
         myTea.prepareRecipe();
 
+        TeaWithHook teaHook = new TeaWithHook();
+        CoffeWithHook coffeeHook = new CoffeWithHook();
+
+        System.out.println("\nMaking tea...");
+        teaHook.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffeeHook.prepareRecipe();
     }
 }
